14533b8fa72484bb27912133ac5a5b04e0060f2c
Kav1shka <103574964+Kav1shka@users.noreply.github.com>
2024-02-07 22:38:52 +0530

    Update general.js

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -18,7 +18,7 @@ public_users.get('/allbooks',function (req, res) {
   // }catch(error){
   //   res.status(500).json({ message: error.message });
   // }
-  res.send(JSON.stringify(books,null,4));
+  // res.send(JSON.stringify(books,null,4));
 });
 
 // Get book details based on ISBN
@@ -38,22 +38,29 @@ public_users.get('/isbn/:isbn',function (req, res) {
 //   catch(error){
 //     res.status(500).json({ message: error.message });
 //   }
-const isbn=req.params.isbn;
-res.send(books[isbn])
+// const isbn=req.params.isbn;
+// res.send(books[isbn]);
   
  });
   
 // Get book details based on author
 public_users.get('/author/:author',function (req, res) {
-  const author=req.params.author;
-  res.send(books[author])
+  try{
+   
+    const author=req.params.author;
+    const authorBooks = books[author];
+    console.log(authorBooks);
+    res.send(books[author])
+  }catch (error) {
+    res.status(500).json({ message: error.message });
+  }
   // return res.status(300).json({message: "based on author"});
 });
 
 // Get all books based on title
 public_users.get('/title/:title',function (req, res) {
-const title=req.params.title;
-res.send(books[title])
+// const title=req.params.title;
+// res.send(books[title])
   
 });
 
