c4cbad7d1a642f736512136ce3c58cec76e21fee
Kav1shka <103574964+Kav1shka@users.noreply.github.com>
2024-02-07 16:06:29 +0530

    2024.2.7

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -11,27 +11,50 @@ public_users.post("/register", (req,res) => {
 });
 
 // Get the book list available in the shop
-public_users.get('/',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+public_users.get('/allbooks',function (req, res) {
+  // try{ 
+  //   const allbooks=books
+  //   res.status(200).json({ message: "All Books List retrieved", allbooks });
+  // }catch(error){
+  //   res.status(500).json({ message: error.message });
+  // }
+  res.send(JSON.stringify(books,null,4));
 });
 
 // Get book details based on ISBN
 public_users.get('/isbn/:isbn',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+//   try{ 
+//  let index=req.params.isbn;
+//  console.log(index);
+//     const bookDetails=books[index];
+//     console.log(bookDetails);
+//     if(index){
+//       res.status(200).json({ message: "According to ISBN Book List retrieved", bookDetails });
+//     }else{
+//       res.status(404).json({ message: "According to ISBN Book NOT Found" });
+//     }
+//     }
+    
+//   catch(error){
+//     res.status(500).json({ message: error.message });
+//   }
+const isbn=req.params.isbn;
+res.send(books[isbn])
+  
  });
   
 // Get book details based on author
 public_users.get('/author/:author',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  const author=req.params.author;
+  res.send(books[author])
+  // return res.status(300).json({message: "based on author"});
 });
 
 // Get all books based on title
 public_users.get('/title/:title',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+const title=req.params.title;
+res.send(books[title])
+  
 });
 
 //  Get book review
